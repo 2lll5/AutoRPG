188183d44d70faa6805cfd6919baf0e922dcd4b0
2lll5 <128884217+2lll5@users.noreply.github.com>
2026-07-15 00:29:54 +0800

    feat: apply responsive styles to game

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import NamedGame from "../components/NamedGame";
+import ResponsiveStyles from "../components/ResponsiveStyles";
 
 export default function Home() {
-  return <NamedGame />;
+  return (
+    <>
+      <NamedGame />
+      <ResponsiveStyles />
+    </>
+  );
 }
